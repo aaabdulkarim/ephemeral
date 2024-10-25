fbbc0732e646757652ee8f87fd84801bebb5a42e
amadeus <aabdulkarim@student.tgm.ac.at>
2024-10-25 04:31:10 +0200

    Correct Finding of Paths. Testing tbc

--- ephemeral-main/logic.py
+++ ephemeral-main/logic.py
@@ -6,25 +6,34 @@ registeredPaths = []
 
 def isEphemeral(dirs, term, root):
     """
-    If File or Directory is Ephimeral it will be added to the registeredPaths list
+    If File or Directory is Ephemeral it will be added to the registeredPaths list
     """
     condition = False
-    for idx, d in enumerate(dirs):
-        str_list = d.split()
+    for d in dirs:
+        str_list = d.split("-")
         # if Term is triggered
-        if idx < len(str_list) - 3:
-            conditionTerm = term.lower() in str_list
-            conditionMinutes = str_list[idx + 1].isnumeric()
-            condtionColon = str_list[idx + 2] == ":" 
-            conditionSeconds = str_list[idx + 3].isnumeric()
+        for idx, str in enumerate(str_list):
             
+            conditionTerm = term.lower() == str
+            if conditionTerm:
+                try:   
+                    minutes = str_list[idx + 1]
+                    conditionMinutes = minutes.isnumeric()
 
-            condition = conditionTerm  and  conditionMinutes and conditionSeconds  
+                    colon = str_list[idx + 2]
+                    condtionColon = colon == ":"
 
-            if condition:
-                print(root, d)
-                registeredPaths.append(root + d)
+                    seconds = str_list[idx + 3]
+                    conditionSeconds = seconds.isnumeric()
+                    
 
+                    condition = conditionTerm  and  conditionMinutes and conditionSeconds  
+
+                    if condition:
+                        print(root, d)
+                        registeredPaths.append(root + d)
+                except IndexError:
+                    continue
     
 
 def checkFiles(term):
@@ -39,13 +48,14 @@ def checkFiles(term):
     # The task of this method is to checregisteredPaths.append(d)k if the Terms are in the file or directory names
     # 
     for root, dirs, files in os.walk("/home/amadeus"):
+        isEphemeral(files, term, root)            
+
         isEphemeral(dirs, term, root)
 
-        isEphemeral(files, term, root)            
 
 
     
 
 
-checkFiles("temp")
+checkFiles("ephemeral")
 
