4a2b49bc86e9665a5e280f25d977b9b66669c9ce
amadeus <aabdulkarim@student.tgm.ac.at>
2024-10-25 04:02:20 +0200

    Playing around with the algorithm

--- ephemeral-main/logic.py
+++ ephemeral-main/logic.py
@@ -1,22 +1,42 @@
 import os
 
+# Saves Directory Pahts and File Paths
+registeredPaths = []
 
-def checkFiles(searchTermList):
+
+def isEphemeral(dirs, term, root):
+    """
+    If File or Directory is Ephimeral it will be added to the registeredPaths list
+    """
+    condition = False
+    for idx, d in enumerate(dirs):
+        str_list = d.split()
+        # if Term is triggered
+        if idx <= len(str_list) - 3:
+            condition = term.lower() in str_list, str_list[idx + 1].isnumeric() and str_list[idx + 2] == ":" and str_list[idx + 3]
+            if condition:
+                print(root, d)
+                registeredPaths.append(root + d)
+
+    
+
+def checkFiles(term):
     """
     Algorithim which will check all files 
     """
-    timer = 0
 
     # Os walk really just walks through every directory. 
     # Every Directory has a root (like the path), 
     # dirs (directories in the current one) and files
     #
-    # The task of this method is to check if the Terms are in the file or directory names
+    # The task of this method is to checregisteredPaths.append(d)k if the Terms are in the file or directory names
     # 
-    for root, dirs, files in os.walk("/"):
-        for d in dirs:
-            for term in searchTermList:
-                if term in d.split("-"):
-                    print(root, d) 
+    for root, dirs, files in os.walk("/home/amadeus"):
+        isEphemeral(dirs, term, root)
 
-checkFiles(["temp"])
+        isEphemeral(files, term, root)            
+
+
+    
+
+checkFiles("ephimeral")
